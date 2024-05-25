b6430158db74021afaee54ab5f16be4f00db515d
Matheus <matheusteodorogarcia@gmail.com>
2024-05-25 16:23:52 -0300

    Sales summary

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,11 +1,38 @@
-import React from "react";
 import { useData } from "../Context/DataContext";
 
 const Resumo = () => {
   const { data } = useData();
 
   console.log(data);
-  return <div>Resumo</div>;
+  if (data === null) return null;
+  return (
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          {data
+            .filter((venda) => venda.status !== "falha")
+            .reduce((acc, venda) => acc + venda.preco, 0)
+            .toLocaleString("pr-br", { style: "currency", currency: "BRL" })}
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          {data
+            .filter((venda) => venda.status === "pago")
+            .reduce((acc, venda) => acc + venda.preco, 0)
+            .toLocaleString("pr-br", { style: "currency", currency: "BRL" })}
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          {data
+            .filter((venda) => venda.status === "processando")
+            .reduce((acc, venda) => acc + venda.preco, 0)
+            .toLocaleString("pr-br", { style: "currency", currency: "BRL" })}
+        </div>
+      </div>
+      <div className="box">Gráficos</div>
+    </section>
+  );
 };
 
 export default Resumo;
