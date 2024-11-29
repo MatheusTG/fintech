9e79aff7688cab6153ff7b7f239ac24c5ec28ca6
Matheus <matheusteodorogarcia@gmail.com>
2024-11-29 09:01:55 -0300

    Organizando Menu no app.jsx

--- src/Components/MesBtn.tsx
+++ src/Components/MesBtn.tsx
@@ -16,7 +16,7 @@ function nomeMes(n: number) {
   date.setMonth(date.getMonth() + n);
   return new Intl.DateTimeFormat("pt-BR", { month: "long" }).format(date);
 }
-
+  
 function formatDate(date: Date) {
   const dd = String(date.getDate()).padStart(2, "0");
   const mm = String(date.getMonth() + 1).padStart(2, "0");
@@ -45,3 +45,5 @@ const MesBtn = ({ n }: { n: number }) => {
 };
 
 export default MesBtn;
+
+
